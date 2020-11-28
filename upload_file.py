0b831340685a6739abbc4fc29546b515d587f34a
Masum <1505014.mr@ugrad.cse.buet.ac.bd>
2020-11-28 11:10:47 +0600

    readme cleared

--- upload_file.py
+++ upload_file.py
@@ -5,8 +5,8 @@ files = {'file': open('sample.pdf', 'rb')}
 values = {'device_id': 'd_1234', 'timestamp':'1606507067'}
 
 
-# url = 'https://bayesbeat.herokuapp.com/file/upload/'
-url = 'http://127.0.0.1:8000/file/upload/'
+url = 'https://bayesbeat.herokuapp.com/file/upload/'
+# url = 'http://127.0.0.1:8000/file/upload/'
 
 response = requests.post(url, files=files, data=values)
 
